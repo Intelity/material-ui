72929161d7f2ec869a19d54f067fff1a49457657
Ilya Shindyapin <ishindyapin@keypr.com>
2017-03-25 12:13:51 -0700

    Change the copy-files script to set a scoped package name
    Signed-off-by: Ilya Shindyapin <ishindyapin@keypr.com>

--- scripts/copy-files.js
+++ scripts/copy-files.js
@@ -58,7 +58,7 @@ function createPackageFile() {
     } = packageData;
 
     const minimalPackage = {
-      name: 'material-ui',
+      name: '@keypr/material-ui',
       author,
       version,
       description,
